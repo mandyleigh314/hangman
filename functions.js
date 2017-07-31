2cbc1d2cfab8f510bcc8b99e2cbd35783ff5d28f
Mandy Jacobsen <Mandy@Knightowltrivia.com>
2017-07-31 11:40:40 -0400

    making progress

--- functions.js
+++ functions.js
@@ -1,19 +1,17 @@
-
 //Getting a random word
-const word = words => {
-    const randIndex = Math.floor(Math.random() * words.length)
-    return words[randIndex]
-    console.log(word)
+const word = dictionary => {
+  const randIndex = Math.floor(Math.random() * dictionary.length)
+  return dictionary[randIndex]
 }
 //Still need to break word down into letters, use split and join?
 
-
 //Creating an array of dashes... how to remove commas?
 let beginning = []
-let start() => {
+function start() {
   for (var i = 0; i < word.length; i++) {
-    beginning.push("_ ")
+    beginning.push('_ ')
   }
 }
 
-module.exports = start, beginning, word
+module.exports = start
+module.exports = word
--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser')
 const fs = require('fs')
 //below I should be calling the functions.js
 const functions = require('./functions.js')
-const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
+const dictionary = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
 
+let count = 8
+let underscores = []
 let guessedLetters = []
 
 app = express()
@@ -18,9 +20,35 @@ app.engine('mst', mustacheExpress())
 app.set('views', './templates')
 app.set('view engine', 'mst')
 
+function randomWord() {
+  let i = Math.floor(Math.random() * dictionary.length)
+  return dictionary[i]
+}
+
+let word = randomWord()
+
+wordInPlay = word.split('')
+
+function start() {
+  for (var i = 0; i < word.length; i++) {
+    underscores.push('_ ')
+  }
+}
+start()
+
 app.get('/', (req, res) => {
-  console.log(words)
-  res.render('index')
+  res.render('index', { count: count, underscores: underscores, guessedLetters: guessedLetters, word: word })
+})
+
+app.post('/', (req, res) => {
+  guessedLetters.push(req.body.guess)
+  console.log(wordInPlay)
+  for (var k = 0; k < wordInPlay.length; k++) {
+    if (wordInPlay[k] === guessedLetters) {
+      underscores.push(wordInPlay[k])
+    } else count - 1
+  }
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
