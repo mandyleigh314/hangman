523e73f8c8f4ce3bfb087cc8edd0956bc15a8ce8
Mandy Jacobsen <Mandy@Knightowltrivia.com>
2017-08-01 09:29:52 -0400

    almost done, with sessions, just need to clear out display when hitting reset

--- main.js
+++ main.js
@@ -2,13 +2,8 @@ const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
-//below I should be calling the functions.js
-const functions = require('./functions.js')
+const expressSession = require('express-session')
 const dictionary = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
-
-let count = 8
-let underscores = []
-let guessedLetters = []
 
 app = express()
 
@@ -16,38 +11,109 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(
+  expressSession({
+    secret: 'Keyboard Cat',
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
 app.engine('mst', mustacheExpress())
 app.set('views', './templates')
 app.set('view engine', 'mst')
 
-function randomWord() {
-  let i = Math.floor(Math.random() * dictionary.length)
-  return dictionary[i]
-}
-
-let word = randomWord()
-
-wordInPlay = word.split('')
-
-function start() {
-  for (var i = 0; i < word.length; i++) {
-    underscores.push('_ ')
-  }
-}
-start()
-
 app.get('/', (req, res) => {
-  res.render('index', { count: count, underscores: underscores, guessedLetters: guessedLetters, word: word })
+  if (!req.session.word) {
+    function randomWord() {
+      let i = Math.floor(Math.random() * dictionary.length)
+      return dictionary[i]
+    }
+
+    req.session.word = randomWord()
+    let word = req.session.word
+    let wordInPlay = req.session.wordInPlay
+
+    req.session.wordInPlay = req.session.word.split('')
+
+    if (!req.session.underscores) {
+      req.session.underscores = []
+    }
+
+    let underscores = req.session.underscores
+
+    function start() {
+      for (var i = 0; i < word.length; i++) {
+        req.session.underscores.push('_')
+      }
+    }
+    start()
+    req.session.count = 8
+  }
+
+  let guessedLetters = req.session.guessedLetters
+  let guesses = req.session.guesses
+  let message = req.session.message
+
+  res.render('index', {
+    count: req.session.count,
+    underscores: req.session.underscores,
+    guessedLetters: req.session.guessedLetters,
+    guesses: req.session.guesses,
+    word: req.session.word,
+    message: req.session.message
+  })
 })
 
 app.post('/', (req, res) => {
-  guessedLetters.push(req.body.guess)
-  console.log(wordInPlay)
-  for (var k = 0; k < wordInPlay.length; k++) {
-    if (wordInPlay[k] === guessedLetters) {
-      underscores.push(wordInPlay[k])
-    } else count - 1
+  req.session.message = ''
+  if (!req.session.guessedLetters) {
+    req.session.guessedLetters = []
   }
+  let guessedLetters = req.session.guessedLetters
+  let guesses = req.session.guesses
+  let underscores = req.session.underscores
+
+  if (guessedLetters.includes(req.body.guess)) {
+    message = 'You have already chosen that letter.'
+    res.redirect('/')
+    return
+  } else if (req.session.wordInPlay.includes(req.body.guess)) {
+    for (let k = 0; k < req.session.wordInPlay.length; k++) {
+      if (req.session.wordInPlay[k] === req.body.guess) {
+        req.session.underscores.splice(k, 1, req.body.guess)
+      }
+    }
+  } else {
+    req.session.count -= 1
+    if (req.session.count === 0) {
+      res.render('loser', {
+        count: req.session.count,
+        underscores: req.session.underscores,
+        guesses: req.session.guesses,
+        word: req.session.word
+      })
+      return
+    }
+  }
+
+  req.session.guessedLetters.push(req.body.guess)
+  req.session.guesses = guessedLetters.slice('').join(' ')
+
+  if (!underscores.includes('_')) {
+    res.render('winner', {
+      count: req.session.count,
+      underscores: req.session.underscores,
+      guesses: req.session.guesses,
+      word: req.session.word
+    })
+    return
+  }
+  res.redirect('/')
+})
+
+app.post('/reset', (req, res) => {
+  req.session.word = null
   res.redirect('/')
 })
 
